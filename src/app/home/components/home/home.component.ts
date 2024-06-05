305367416bcc598ba4ae12148c53ba46729d4588
Marko2073 <95702488+Marko2073@users.noreply.github.com>
2024-06-05 23:28:48 +0200

    full comit

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../../../products/buisness-logic/api/products.service";
+import { Product } from "../../../products/interfaces/product";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'] // Promenjeno iz styleUrl u styleUrls
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  featuredProducts: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.selectFeaturedProducts();
+    });
+  }
+
+  private getSeededRandom(seed: number): number {
+    const x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
+  }
+
+  private selectFeaturedProducts() {
+    const today = new Date();
+    const seed = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+    const uniqueIndices = new Set<number>();
+
+    while (uniqueIndices.size < 4 && uniqueIndices.size < this.products.length) {
+      const randomIndex = Math.floor(this.getSeededRandom(seed + uniqueIndices.size) * this.products.length);
+      uniqueIndices.add(randomIndex);
+    }
+
+    this.featuredProducts = Array.from(uniqueIndices).map(index => this.products[index]);
+  }
+  AddToCart(event:any, id:string){
+
+    let cart: any[] = [];
+    let item = {
+      id: id,
+      quantity: 1
+    };
+    let cartData = localStorage.getItem('cart');
+    if(cartData == null){
+      cart.push(item);
+      localStorage.setItem('cart', JSON.stringify(cart));
+
+
+    } else {
+      let index = -1;
+      cart = JSON.parse(cartData);
+      for(let i = 0; i < cart.length; i++){
+        if(cart[i].id === id){
+          index = i;
+          break;
+        }
+      }
+      if(index === -1){
+        cart.push(item);
+        localStorage.setItem('cart', JSON.stringify(cart));
+      } else {
+        cart[index].quantity += 1;
+        localStorage.setItem('cart', JSON.stringify(cart));
+      }
+    }
+
+  }
 }
--- src/app/single/components/single/single.component.ts
+++ src/app/single/components/single/single.component.ts
@@ -48,7 +48,38 @@ export class SingleComponent implements OnInit {
       });
     });
   }
+  AddToCart(event:any, id:string){
+    let cart: any[] = [];
+    let item = {
+      id: id,
+      quantity: 1
+    };
+    let cartData = localStorage.getItem('cart');
+    if(cartData == null){
+      cart.push(item);
+      localStorage.setItem('cart', JSON.stringify(cart));
 
 
+    } else {
+      let index = -1;
+      cart = JSON.parse(cartData);
+      for(let i = 0; i < cart.length; i++){
+        if(cart[i].id === id){
+          index = i;
+          break;
+        }
+      }
+      if(index === -1){
+        cart.push(item);
+        localStorage.setItem('cart', JSON.stringify(cart));
+      } else {
+        cart[index].quantity += 1;
+        localStorage.setItem('cart', JSON.stringify(cart));
+      }
+    }
 
+  }
+
+
+  protected readonly event = event;
 }
