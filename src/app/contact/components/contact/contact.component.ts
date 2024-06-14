45df42ff96152bb8628e372b464f7da4c989c92d
Marko2073 <95702488+Marko2073@users.noreply.github.com>
2024-06-14 09:14:15 +0200

    contact update modal

--- src/app/contact/components/contact/contact.component.ts
+++ src/app/contact/components/contact/contact.component.ts
@@ -1,53 +1,51 @@
-import {Component} from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
-import {ContactService} from "../../buisness-logic/api/contact.service";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { FormControl, Validators } from "@angular/forms";
+import { ContactService } from "../../buisness-logic/api/contact.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
-
 })
 export class ContactComponent {
-  constructor(private contactService: ContactService, private router: Router) {
-  }
+  constructor(private contactService: ContactService, private router: Router) {}
 
-
-  value = "ContactComponent";
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
   nameFormControl = new FormControl('', [Validators.required]);
   subjectFormControl = new FormControl('', [Validators.required]);
   MessageFormControl = new FormControl('', [Validators.required]);
 
   formSend() {
-    if (this.emailFormControl.valid && this.nameFormControl.valid && this.subjectFormControl.valid && this.MessageFormControl.valid) {
-
-            this.contactService.postMessage({
-              email: this.emailFormControl.value,
-              name: this.nameFormControl.value,
-              subject: this.subjectFormControl.value,
-              message: this.MessageFormControl.value
-            }).subscribe({
-              next: (data) => {
-                console.log(this.emailFormControl.value);
-                this.router.navigate(['/home']);
-
-
-
-
-        },
-        error: (err) => {
-          console.log(err);
-
-        }
-      });
-
-
-    } else {
-      alert("Please fill in all the fields");
+    if (
+      this.emailFormControl.valid &&
+      this.nameFormControl.valid &&
+      this.subjectFormControl.valid &&
+      this.MessageFormControl.valid
+    ) {
+      this.contactService
+        .postMessage({
+          email: this.emailFormControl.value,
+          name: this.nameFormControl.value,
+          subject: this.subjectFormControl.value,
+          message: this.MessageFormControl.value,
+        })
+        .subscribe({
+          next: (data) => {
+            console.log(this.emailFormControl.value);
+            this.showModal();
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
     }
+  }
 
-
+  showModal() {
+    const modal = document.getElementById('successModal');
+    if (modal) {
+      modal.style.display = 'block';
+    }
   }
 }
